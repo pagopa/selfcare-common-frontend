da9acf6da24e5f3b3b2201e0229ff891f79d24e9
anttorre <antonio.torre@everis.com>
2022-01-19 19:20:39 +0100

    fixed export in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,11 @@
-import './components/Header/Header';
-import './components/Footer/Footer';
+export * from './components/Header/Header';
+export * from './components/Footer/Footer';
+export * from './components/CustomAvatar';
+export * from './components/CustomPagination';
+export * from './components/FilterModal';
+export * from './components/SessionModal';
+export * from './components/TitleBox';
+export * from './components/Toast';
+
+export * from './components/ErrorBoundary/ErrorBoundary';
+export * from './components/Loading/LoadingOverlay';
