0e75b9a0c60e7800878c1f7d923253c7cea8510a
anttorre <antonio.torre@everis.com>
2022-02-03 12:56:45 +0100

    SELC-450 filterModal will discard selected when closed

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,31 +1,14 @@
 export { default as Header } from './components/Header/Header';
-export * from './components/Header/Header';
-
 export { default as Footer } from './components/Footer/Footer';
-export * from './components/Footer/Footer';
-
 export { default as CustomAvatar } from './components/CustomAvatar';
-export * from './components/CustomAvatar';
-
 export { default as CustomPagination } from './components/CustomPagination';
-export * from './components/CustomPagination';
-
 export { default as FilterModal } from './components/FilterModal';
-export * from './components/FilterModal';
-
 export { default as SessionModal } from './components/SessionModal';
-export * from './components/SessionModal';
-
 export { default as TitleBox } from './components/TitleBox';
-export * from './components/TitleBox';
-
 export { default as Toast } from './components/Toast';
-export * from './components/Toast';
+export { default as EndingPage } from './components/EndingPage';
 
 export { default as ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
-export * from './components/ErrorBoundary/ErrorBoundary';
-
 export { default as LoadingOverlay } from './components/Loading/LoadingOverlay';
-export * from './components/Loading/LoadingOverlay';
-
-export { default as EndingPage } from './components/EndingPage';
+export { default as UserNotifyHandle } from './components/UserNotifyHandle';
+export { default as UnloadEventHandler } from './components/UnloadEventHandler';
