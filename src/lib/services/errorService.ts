f93cd19ac91cfd74c910c7446c2640634bec96d7
anttorre <antonio.torre@everis.com>
2022-02-04 10:21:37 +0100

    analyticsService tested

--- src/lib/services/errorService.ts
+++ src/lib/services/errorService.ts
@@ -1,10 +1,10 @@
 import { AppError } from '../redux/slices/appStateSlice';
-import { GENERIC_EVENT, trackEvent } from './analyticsService';
+import { trackAppError } from './analyticsService';
 
 export const handleErrors = (errors: Array<AppError>) => {
   errors
     .filter((e) => e.toNotify)
     .forEach((e) => {
-      trackEvent(GENERIC_EVENT, e);
+      trackAppError(e);
     });
 };
