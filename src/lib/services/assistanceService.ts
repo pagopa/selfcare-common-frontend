2d03c2744db0d8822557c66ede721cc3387ae757
USERSAD\esignore <elena.signore@nttdata.com>
2022-02-07 17:58:13 +0100

    SELC-144-track-events - added track events in common

--- src/lib/services/assistanceService.ts
+++ src/lib/services/assistanceService.ts
@@ -3,12 +3,14 @@ import isEmpty from 'lodash/isEmpty';
 import { CONFIG } from '../config/env';
 import { storageRead } from '../utils/storage-utils';
 import { STORAGE_KEY_USER } from '../utils/constants';
+import { trackEvent } from './analyticsService';
 
 export const buildAssistanceURI = (assistanceEmail?: string) => {
 
     const sessionStorageUser = storageRead(STORAGE_KEY_USER, 'object');
     
     if (isEmpty(sessionStorageUser)) {
+        trackEvent('CUSTOMER_CARE_MAILTO', { event_name: 'CUSTOMER_CARE_MAILTO' });
         return `mailto:${assistanceEmail}`;
     }  else {
         return CONFIG.URL_FE.ASSISTANCE;
