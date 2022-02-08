c78ff83a5143a00dbd0393526561e2f665407809
anttorre <antonio.torre@everis.com>
2022-02-08 19:28:06 +0100

    fixed CUSTOMER_CARE_MAILTO event payload

--- src/lib/services/assistanceService.ts
+++ src/lib/services/assistanceService.ts
@@ -1,4 +1,3 @@
-
 import isEmpty from 'lodash/isEmpty';
 import { CONFIG } from '../config/env';
 import { storageRead } from '../utils/storage-utils';
@@ -6,13 +5,12 @@ import { STORAGE_KEY_USER } from '../utils/constants';
 import { trackEvent } from './analyticsService';
 
 export const buildAssistanceURI = (assistanceEmail?: string) => {
+  const sessionStorageUser = storageRead(STORAGE_KEY_USER, 'object');
 
-    const sessionStorageUser = storageRead(STORAGE_KEY_USER, 'object');
-    
-    if (isEmpty(sessionStorageUser)) {
-        trackEvent('CUSTOMER_CARE_MAILTO', { event_name: 'CUSTOMER_CARE_MAILTO' });
-        return `mailto:${assistanceEmail}`;
-    }  else {
-        return CONFIG.URL_FE.ASSISTANCE;
-    }
+  if (isEmpty(sessionStorageUser)) {
+    trackEvent('CUSTOMER_CARE_MAILTO');
+    return `mailto:${assistanceEmail}`;
+  } else {
+    return CONFIG.URL_FE.ASSISTANCE;
+  }
 };
