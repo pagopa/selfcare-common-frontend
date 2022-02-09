46f29b612910ab88c0d1ed1b3735fb7cc48b1ec4
USERSAD\esignore <elena.signore@nttdata.com>
2022-02-09 18:48:52 +0100

    edit-rolelabel - added description in roleLabels

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -8,7 +8,7 @@ export const STORAGE_KEY_TOKEN = 'token';
 export const LOADING_TASK_LOGIN_CHECK = 'LOGIN_CHECK';
 
 /** The short and long labels used for the roles of selfcare's projects */
-export const roleLabels: { [key in UserRole]: { shortLabel: string; longLabel: string } } = {
-  ADMIN: { shortLabel: 'Ref. Amministrativo', longLabel: 'Referente Amministrativo' },
-  LIMITED: { shortLabel: 'Ref. Operativo', longLabel: 'Referente Operativo' },
+export const roleLabels: { [key in UserRole]: { shortLabel: string; longLabel: string; description:string } } = {
+  ADMIN: { shortLabel: 'Ref. Amministrativo', longLabel: 'Referente Amministrativo',description:'Gestisce i prodotti e gli utenti attivi' },
+  LIMITED: { shortLabel: 'Ref. Operativo', longLabel: 'Referente Operativo', description:'Operatore prodotto' },
 };
