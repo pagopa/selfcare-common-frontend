538dac8912ae4eac85ce410adc517bc1d7a63a0c
USERSAD\esignore <elena.signore@nttdata.com>
2022-03-16 19:12:33 +0100

    texts-translation updated

--- src/examples/TranslationTextExample.tsx
+++ src/examples/TranslationTextExample.tsx
@@ -8,7 +8,7 @@ import {
   DialogTitle,
 } from '@mui/material';
 import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 // import { SessionModal } from '../lib';
 
 export default function TranslationTextExample() {
@@ -21,7 +21,9 @@ export default function TranslationTextExample() {
   const handleClose = () => {
     setOpen(false);
   };
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const obj = { name: 'Jon', surname: 'Smith' };
+
   return (
     <>
       <Button onClick={handleClickOpen} variant="contained">
@@ -51,6 +53,54 @@ export default function TranslationTextExample() {
               Test Descrizione con <strong>{t('boldDescriptionText')}</strong>
             </Box>
             <Box>{t('boldDescriptionTwo')}</Box>
+            <Box>{t('description.part1')}</Box>
+            <Box>
+              {t('key', {
+                what: 'prima parte',
+                how: 'seconda parte',
+              })}
+            </Box>
+            -{/* with bold */}
+            <Box>
+              {t('key', {
+                what: 'prima parte ',
+                e: '',
+                how: '',
+              })}
+              <strong>
+                {t('key', {
+                  what: '',
+                  e: 'e',
+                  how: 'seconda parte bold',
+                })}
+              </strong>
+            </Box>
+            {/* <Box> // non funziona
+              {t('key', {
+                what: 'prima parte',
+                how: (
+                  <>
+                    <strong>test</strong>
+                  </>
+                ),
+              })}
+            </Box> */}
+            <Box>
+              <Trans i18nKey="key2">
+                {/* xx: is the default text */}
+                xx <strong>{{ author: `${obj.name} ${obj.surname}` }}</strong>
+              </Trans>
+            </Box>
+            <Box>{t('nesting1')}</Box>
+            {/* joinArray concatena gli elementi dell'arrai con quanto definito dopo, in questo caso uno sapzio. joinArrays: ',' inserisce una virgola */}
+            <Box>{t('arrayJoinWithInterpolation', { myVar: 'interpolate', joinArrays: ' ' })}</Box>
+            <Box>
+              {t('arrayOfObjects.0.name')}
+              <strong>{t('arrayOfObjects.1.name')}</strong>
+            </Box>
+            <Button onClick={() => i18n.changeLanguage(i18n.language === 'it' ? 'en' : 'it')}>
+              {i18n.language}
+            </Button>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,6 @@ import { initReactI18next } from 'react-i18next';
 const labelTitle = 'nome Titolo';
 const descriptionEn = 'bold description';
 const descriptionIt = 'descrizione in grassetto';
-
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
@@ -15,6 +14,17 @@ const resources = {
       labelTitle: 'name Title',
       boldDescriptionText: ' Test Description with bold text',
       boldDescriptionTwo: `view ${descriptionEn}`,
+      description: {
+        part1: 'eng: description test-part1',
+        part2: 'eng: description test-part2',
+      },
+      key: '{{what}} {{and}} {{how}}!!',
+      key2: 'I am <1>{{author}}</1> not bold',
+      nesting1: '1 $t(nesting2)',
+      nesting2: '2 $t(nesting3)',
+      nesting3: '3',
+      arrayJoinWithInterpolation: ['you', 'can', '{{myVar}}'],
+      arrayOfObjects: [{ name: 'tom' }, { name: 'steve' }],
     },
   },
   it: {
@@ -23,6 +33,17 @@ const resources = {
       labelTitle: `${labelTitle}`,
       boldDescriptionText: ' testo in grassetto',
       boldDescriptionTwo: `visualizza la ${descriptionIt}`,
+      description: {
+        part1: 'ita: descrizione test-parte1',
+        part2: 'ita: descrizione test-parte2',
+      },
+      key: '{{what}} {{e}} {{how}}!!',
+      key2: 'sono <1>{{author}}</1> non in grassetto',
+      nesting1: '1 $t(nesting2)',
+      nesting2: '2 $t(nesting3)',
+      nesting3: '3',
+      arrayJoinWithInterpolation: ['tu', 'puoi', '{{myVar}}'],
+      arrayOfObjects: [{ name: 'tom' }, { name: ' steve' }],
     },
   },
 };
@@ -32,7 +53,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'it', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: 'it', // en it / language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
