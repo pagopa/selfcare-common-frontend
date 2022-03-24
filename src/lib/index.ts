f4273dba748c521ca9aeadb42a5c9344a1b785c2
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2022-03-24 17:22:24 +0100

    Added LoadingOverlayComponent (#58)
    Co-authored-by: anttorre <antonio.torre@everis.com>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,9 +6,39 @@ export { default as FilterModal } from './components/FilterModal';
 export { default as SessionModal } from './components/SessionModal';
 export { default as TitleBox } from './components/TitleBox';
 export { default as Toast } from './components/Toast';
+export { default as ToastWrapper } from './components/ToastWrapper';
 export { default as EndingPage } from './components/EndingPage';
+export { default as LoadingOverlayComponent } from './components/LoadingOverlayComponent';
+export { default as NavigationBar } from './components/NavigationBar';
+
+export { default as CheckIllustrationIcon } from './components/icons/CheckIllustrationIcon';
+export { default as ConfirmIcon } from './components/icons/ConfirmIcon';
+export { default as ErrorIcon } from './components/icons/ErrorIcon';
+export { default as FaultIcon } from './components/icons/FaultIcon';
+export { default as PagoPaIcon } from './components/icons/PagoPaIcon';
+export { default as PagoPaMiniIcon } from './components/icons/PagoPaMiniIcon';
 
 export { default as ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
+export { default as useErrorDispatcher } from './hooks/useErrorDispatcher';
+
 export { default as LoadingOverlay } from './components/Loading/LoadingOverlay';
+export { default as useLoading } from './hooks/useLoading';
+
 export { default as UserNotifyHandle } from './components/UserNotifyHandle';
+export { default as useUserNotify } from './hooks/useUserNotify';
+
 export { default as UnloadEventHandler } from './components/UnloadEventHandler';
+export {
+  useUnloadEventInterceptor,
+  useUnloadEventInterceptorAndActivate,
+  useUnloadEventOnExit,
+  useUnloadEventLogout,
+} from './hooks/useUnloadEventInterceptor';
+
+export { default as withLogin } from './decorators/withLogin';
+export { useLogin } from './hooks/useLogin';
+
+export { default as withRetrievedValue } from './decorators/withRetrievedValue';
+export { default as useReduxCachedValue } from './hooks/useReduxCachedValue';
+
+export { default as useFakePagination } from './hooks/useFakePagination';
