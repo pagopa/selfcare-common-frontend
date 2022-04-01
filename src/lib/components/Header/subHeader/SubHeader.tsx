ad64029c4d249815a3c400f9f508c18a8277012c
USERSAD\esignore <elena.signore@nttdata.com>
2022-04-01 18:02:35 +0200

    SELC-222 changed color in Footer Header and subHeader

--- src/lib/components/Header/subHeader/SubHeader.tsx
+++ src/lib/components/Header/subHeader/SubHeader.tsx
@@ -1,51 +1,58 @@
 import React from 'react';
-import { AppBar, Grid, Toolbar, Typography} from '@mui/material';
+import { AppBar, Grid, Toolbar, Typography } from '@mui/material';
 
-type Props={
-  children?:React.ReactNode;
+type Props = {
+  children?: React.ReactNode;
 };
 
-export default function SubHeader({children}: Props) { 
+export default function SubHeader({ children }: Props) {
   return (
     <AppBar
-    position="relative"
-    sx={{ alignItems: 'center', height: '107px', backgroundColor: '#0066CC', boxShadow:'none' }}
-  >
-    <Toolbar sx={{ width: { xs: '100%', lg: '90%' }, minHeight: '107px !important' }}>
-      <Grid container>
-        <Grid container item direction="column" xs={6}>
-          <Grid item>
-            <Typography
-              component="div"
-              sx={{
-                fontWeight: 'bold',
-                fontSize: '24px',
-                lineHeight: '36px',
-                textAlign: 'left',
-                color: 'background.default',
-              }}
-            >
-              Self Care
-            </Typography>
+      position="relative"
+      sx={{
+        alignItems: 'center',
+        height: '107px',
+        backgroundColor: 'white',
+        boxShadow: 'none',
+        borderTop: '1px solid #F2F2F2',
+        borderBottom: '1px solid #F2F2F2',
+      }}
+    >
+      <Toolbar sx={{ width: { xs: '100%', lg: '90%' }, minHeight: '107px !important' }}>
+        <Grid container>
+          <Grid container item direction="column" xs={6}>
+            <Grid item>
+              <Typography
+                component="div"
+                sx={{
+                  fontWeight: 'bold',
+                  fontSize: '24px',
+                  lineHeight: '36px',
+                  textAlign: 'left',
+                  color: '#17324D',
+                }}
+              >
+                Self Care
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                component="div"
+                sx={{
+                  fontWeight: 'normal',
+                  fontSize: '14px',
+                  lineHeight: '24px',
+                  textAlign: 'left',
+                  color: '#17324D',
+                }}
+              >
+                Gestisci i tuoi prodotti e servizi PagoPA
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Typography
-              component="div"
-              sx={{
-                fontWeight: 'normal',
-                fontSize: '14px',
-                lineHeight: '24px',
-                textAlign: 'left',
-                color: 'background.default',
-              }}
-            >
-              Gestisci i tuoi prodotti e servizi PagoPA
-            </Typography>
-          </Grid>
+          {children}
         </Grid>
-        {children}
-      </Grid>
-    </Toolbar>
-  </AppBar>
+      </Toolbar>
+    </AppBar>
   );
 }
