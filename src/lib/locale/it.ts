ee9c96014eb030b9bc82f0b491e2ff044f7aa931
loremedda <lorenzo.medda.sa@nttdata.com>
2022-04-28 19:23:31 +0200

    change admin and limited labels + localization

--- src/lib/locale/it.ts
+++ src/lib/locale/it.ts
@@ -37,5 +37,17 @@ export default {
       message: 'Se esci, le modifiche apportate andranno perse.',
       confirmLabel: 'Esci',
     },
+    roles: {
+      admin: {
+        shortLabelKey: 'Ref. Amministrativo',
+        longLabelKey: 'Amministratore',
+        descriptionKey: 'Ha tutti i permessi e gestisce gli utenti',
+      },
+      limited: {
+        shortLabelKey: 'Ref. Operativo',
+        longLabelKey: 'Operatore',
+        descriptionKey: "Gestisce l'integrazione tecnologica e/o l'operatività dei servizi",
+      },
+    },
   },
 };
