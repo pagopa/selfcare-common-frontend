c3d3c1a9438e8ec88599c27b808bc37b8e706d2c
loremedda <lorenzo.medda.sa@nttdata.com>
2022-04-28 19:26:00 +0200

    temp

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,25 +1,20 @@
-import { useTranslation } from 'react-i18next';
-
 export const LOADING_TASK_LOGIN_CHECK = 'LOGIN_CHECK';
 export const LOADING_TASK_RETRIEVE_CACHED_VALUES = 'RETRIEVE_CACHED_VALUES';
 
 export type UserRole = 'ADMIN' | 'LIMITED';
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const { t } = useTranslation();
 
 /** The short and long labels used for the roles of selfcare's projects */
 export const roleLabels: {
   [key in UserRole]: { shortLabel: string; longLabel: string; description: string };
 } = {
   ADMIN: {
-    shortLabel: t('common.roles.admin.shortLabelKey'),
-    longLabel: t('common.roles.admin.longLabelKey'),
-    description: t('common.roles.admin.description'),
+    shortLabel: 'common.roles.admin.shortLabelKey',
+    longLabel: 'common.roles.admin.longLabelKey',
+    description: 'common.roles.admin.description',
   },
   LIMITED: {
-    shortLabel: t('common.roles.limited.shortLabelKey'),
-    longLabel: t('common.roles.limited.longLabelKey'),
-    description: t('common.roles.limited.description'),
+    shortLabel: 'common.roles.limited.shortLabelKey',
+    longLabel: 'common.roles.limited.longLabelKey',
+    description: 'common.roles.limited.description',
   },
 };
