0747c54224f54b41a0cb5faced0cccefcfdffec7
loremedda <98110186+loremedda@users.noreply.github.com>
2023-01-16 18:33:13 +0100

    SELC-1884 Created a function that check the insertedChecksum based on the calculation algorithm + unitTests (#136)

---
+++ src/lib/utils/__tests__/verifyChecksumMatchWithTaxCode.test.ts
@@ -0,0 +1,22 @@
+import { User } from '../../model/User';
+import { verifyChecksumMatchWithTaxCode } from '../verifyChecksumMatchWithTaxCode';
+
+const user: User = {
+  name: 'MARIO',
+  surname: 'ROSSI',
+  uid: 'UID23',
+  taxCode: 'MRARSS80A01A794I',
+  email: 'mario.rossi@comunedi.it',
+};
+
+test('test: taxcode checksum is correct, so the function return false (not error)', () => {
+  const result = verifyChecksumMatchWithTaxCode(user.taxCode);
+
+  expect(result).toBeFalsy();
+});
+
+test('test: taxcode checksum is incorrect, so the function return true (error)', () => {
+  const result = verifyChecksumMatchWithTaxCode('MRARSS80A01A794V');
+
+  expect(result).toBeTruthy();
+});
