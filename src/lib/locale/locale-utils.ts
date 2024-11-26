75425a4dac733dd5205421bd24bf4852fece9be8
loremedda <98110186+loremedda@users.noreply.github.com>
2024-11-26 12:07:39 +0100

    [SELC-6042] feat: Added language detection

--- src/lib/locale/locale-utils.ts
+++ src/lib/locale/locale-utils.ts
@@ -1,4 +1,5 @@
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import i18n from 'i18next';
 import it from './it';
 import en from './en';
@@ -20,11 +21,16 @@ export const configureI18n = (
   };
   i18n
     .use(initReactI18next)
+    .use(LanguageDetector)
     .init({
       resources: completeResources,
       lng: defaultLanguage,
       interpolation: {
         escapeValue: false,
+      },
+      detection: {
+        order: ['querystring', 'sessionStorage', 'navigator'],
+        lookupSessionStorage: 'i18nextLng',
       },
     })
     .catch((err) => {
