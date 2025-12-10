2642a95052711cb3614add8e19a07e355119fe14
Pierluigi D'Uffizi <DFFPLG91T09H501X@sogeiesterni.it>
2025-12-10 15:13:58 +0100

    [SELC-8066] feat: add live region label to announce loading in LoadingOverlayComponent

--- src/lib/components/LoadingOverlayComponent.tsx
+++ src/lib/components/LoadingOverlayComponent.tsx
@@ -4,14 +4,22 @@ import MDSpinner from 'react-md-spinner';
 
 type Props = {
   open: boolean;
+  loadingMessage?: string;
 };
 
 /** A component which render an overlay loading */
-export default function LoadingOverlayComponent({ open }: Props) {
+export default function LoadingOverlayComponent({
+  open,
+  loadingMessage = 'Caricamento in corso'
+}: Props) {
   const theme = useTheme();
 
   return (
-    <Modal open={open} sx={{ outline: 0, zIndex: 1500 }}>
+    <Modal
+      open={open}
+      sx={{ outline: 0, zIndex: 1500 }}
+      aria-labelledby="loading-modal-title"
+    >
       <Box
         sx={{
           position: 'absolute',
@@ -20,7 +28,27 @@ export default function LoadingOverlayComponent({ open }: Props) {
           width: '100px',
           outline: 0,
         }}
+        role="status"
+        aria-live="assertive"
+        aria-atomic="true"
+        aria-busy="true"
       >
+        <span
+          id="loading-modal-title"
+          style={{
+            position: 'absolute',
+            width: '1px',
+            height: '1px',
+            padding: '0',
+            margin: '-1px',
+            overflow: 'hidden',
+            clip: 'rect(0,0,0,0)',
+            whiteSpace: 'nowrap',
+            border: '0'
+          }}
+        >
+          {loadingMessage}
+        </span>
         <MDSpinner singleColor={theme.palette.primary.main} role="loadingSpinner" />
       </Box>
     </Modal>
