d8103bab83fe5ce20aca87356bc14761546bb95d
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2026-01-23 18:52:46 +0100

    fix build

--- src/examples/redux/store.ts
+++ src/examples/redux/store.ts
@@ -1,30 +1,34 @@
 import type { Middleware } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+
 import { appStateReducer } from '../../lib/redux/slices/appStateSlice';
 import { permissionsReducer } from '../../lib/redux/slices/permissionsSlice';
 import { userReducer } from '../../lib/redux/slices/userSlice';
 import { testReducer } from './slices/testSlice';
 
-export const createStore = () =>
-  configureStore({
-    reducer: {
-      user: userReducer,
-      appState: appStateReducer,
-      test: testReducer,
-      permissions: permissionsReducer,
-    },
-    middleware: (getDefaultMiddleware) => {
-      const middleware = getDefaultMiddleware({
-        serializableCheck: false,
-      });
+export const createStore = configureStore({
+  reducer: {
+    user: userReducer,
+    appState: appStateReducer,
+    test: testReducer,
+    permissions: permissionsReducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    const middleware = getDefaultMiddleware({
+      serializableCheck: false,
+    });
 
-      if (process.env.NODE_ENV === 'development') {
-        middleware.push(logger as Middleware);
-      }
+    if (process.env.NODE_ENV === 'development') {
+      middleware.push(logger as Middleware);
+    }
 
-      return middleware;
-    },
-  });
+    return middleware;
+  },
+});
 
-  export type RootState = ReturnType<ReturnType<typeof createStore>['getState']>;
+// ---------------------------
+// Typed helpers
+// ---------------------------
+
+export type RootState = ReturnType<typeof createStore.getState>;
