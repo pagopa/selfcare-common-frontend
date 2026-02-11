b3c4f19d05af54e241c2b6adca121b47b734a997
OraldoDoci <oraldo.doci@nttdata.com>
2026-02-11 21:25:15 +0100

    lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import pkg from './package.json';
